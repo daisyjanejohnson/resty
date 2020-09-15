c24f230377524553c586402679bef856125d1151
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-09-14 22:50:36 -0600

    lab 26 finished

---
+++ resty/src/components/footer/footer.js
@@ -0,0 +1,11 @@
+//Footer Component Function
+
+import React from 'react';
+
+function Footer() {
+  return (<footer className="App-footer">
+    <p>@ 2020 Daisy Johnson</p>
+  </footer>)
+}
+
+export default Footer;
