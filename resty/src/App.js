19a78b2a0a27f0264c37e00d6a318d93352e6603
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-09-15 21:40:21 -0600

    almost done with lab27

--- resty/src/App.js
+++ resty/src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import Header from './components/header/header.js';
 import Form from './components/form/form.js';
+import Results from './components/results/results.js';
 import Footer from './components/footer/footer.js';
+// import JSONPretty from 'react-json-pretty';
 import './App.css';
 
-// App component
-function App() {
-  return (
-    <div className="App">
-      {/* HEADER COMPONENT */}
+class App extends React.Component{
+ // By calling super(props) you are calling the constructor of React.Component. Super references parent class.
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      results: [],
+      loading: false,
+    };
+  }
+
+
+  // Every time a change occurs in our input, we call handleForm to output our data in the desired way.
+  handleForm = (count, results) => {
+    this.setState({ count, results });
+  };
+
+  // We are returning these results in our desired format, and rendering them to the app.
+  render(){
+    return(
+      <>
       <Header />
-      <Form />
+      <Form prompt="Enter API URL"  handler={this.handleForm} />
+      <Results count={this.state.count}results={this.state.results}  />
+
       <Footer />
-    </div>
-  );
+      </>
+    )
+  }
 }
 
 
