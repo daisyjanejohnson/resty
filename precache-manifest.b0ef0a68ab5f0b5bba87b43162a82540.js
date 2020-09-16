1808fa84e63c6a159ce7b9a600470ff83ee0ad3b
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-09-15 23:15:10 -0600

    Updates

---
+++ precache-manifest.b0ef0a68ab5f0b5bba87b43162a82540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a229e6a6d3a679383d16fa3b953488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adce72c9362c83279c69",
+    "url": "/static/css/main.f01d11ed.chunk.css"
+  },
+  {
+    "revision": "f5ecc7e20a676ae86483",
+    "url": "/static/js/2.945aa3c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.945aa3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adce72c9362c83279c69",
+    "url": "/static/js/main.b3973abb.chunk.js"
+  },
+  {
+    "revision": "e7b4ea4f970612a52c32",
+    "url": "/static/js/runtime-main.3acf32b8.js"
+  }
+]);
