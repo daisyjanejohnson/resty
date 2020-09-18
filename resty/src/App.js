81c8bfa6c1e75930164d0b9faa10373501f4517c
daisyjane johnson <daisyjane.johnson@gmail.com>
2020-09-17 22:20:47 -0600

    functionality is working and very basic test is passing, needs styling still

--- resty/src/App.js
+++ resty/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+import md5 from 'md5';
+
 import Header from './components/header/header.js';
 import Form from './components/form/form.js';
+import History from './components/history/history.js'
 import Results from './components/results/results.js';
 import Footer from './components/footer/footer.js';
-// import JSONPretty from 'react-json-pretty';
 import './App.css';
 
 class App extends React.Component{
@@ -14,21 +17,75 @@ class App extends React.Component{
       count: 0,
       headers: null,
       results: [],
+      loading: false,
+      request: {},
+      history:{},
     };
   }
 
-  // Every time a change occurs in our input, we call handleForm to output our data in the desired way.
-  handleForm = (headers, count, results) => {
-    this.setState({count, results, headers });
-  };
+  // LOADING FUNCTION Toggles it to be the opposite of what it curently is
+  toggleLoading = () => {
+    this.setState({ loading: !this.state.loading});
+  }
+
+  // GET THE RESULTS
+  updateResults = (headers, results) => {
+    this.setState({ headers, results });
+  }
+
+  // Get the request
+  updateRequest = (request) => {
+    this.setState({ request });
+  }
+
+updateHistory = (request) => {
+  let hash = md5(JSON.stringify(request));
+
+  // new history contains all of the old history
+  const history = {...this.state.history, [hash]:request }
+
+  this.setState({ history }, () => {
+    // grab the history from the state and stringify and it throw it in there with a key of history
+    localStorage.setItem('history', JSON.stringify(this.state.history));
+  });
+}
+
+  fetchResults = async (request) => {
+    try {
+      // Start Load
+      this.toggleLoading();
+
+      this.updateRequest(request);
+
+      let response = await axios(request);
+      //Stop load
+      this.toggleLoading();
+
+      this.updateHistory(request);
+
+      this.updateResults(response.headers, response.data);
+    }
+    catch(e){
+      console.log('ERROR', e);
+    }
+ }
+    componentDidMount() {
+      let history = JSON.parse(localStorage.getItem('history'));
+      this.setState({ history });
+    }
+
+ 
 
   // We are returning these results in our desired format, and rendering them to the app.
   render(){
     return(
       <>
       <Header />
-      <Form prompt="Enter API URL"  handler={this.handleForm} />
-      <Results count={this.state.count} results={this.state.results} headers={this.state.headers}/>
+      <Form prompt="Enter API URL" request={this.state.request} handler={this.fetchResults} />
+      <main>
+      <History handler={this.updateRequest} calls={this.state.history}/>
+      <Results loading={this.state.loading} count={this.state.count} results={this.state.results} headers={this.state.headers}/>
+      </main>
       <Footer />
       </>
     )
